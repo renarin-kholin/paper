d66aa4c1de65580b818a1673bda94d6a628ce6d5
Swaroop Patil <swarooppatil850@gmail.com>
2026-03-13 18:53:16 +0530

    feat: write contract & tests (#1)

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 import "./DeployHelpers.s.sol";
-import { DeployYourContract } from "./DeployYourContract.s.sol";
+import { DeployPostNFT } from "./DeployPostNFT.s.sol";
 
 /**
  * @notice Main deployment script for all contracts
@@ -12,11 +12,13 @@ import { DeployYourContract } from "./DeployYourContract.s.sol";
  */
 contract DeployScript is ScaffoldETHDeploy {
     function run() external {
+        // Child deploy scripts own the broadcast lifecycle via ScaffoldEthDeployerRunner.
+        // Keeping this parent script non-broadcasting avoids nested vm.startBroadcast reverts.
         // Deploys all your contracts sequentially
         // Add new deployments here when needed
 
-        DeployYourContract deployYourContract = new DeployYourContract();
-        deployYourContract.run();
+        DeployPostNFT deployPostNFT = new DeployPostNFT();
+        deployPostNFT.run();
 
         // Deploy another contract
         // DeployMyContract myContract = new DeployMyContract();
